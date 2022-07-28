ff305ed3c95228399a1166dac0b3f31c29593ff1
pranjal bhadu <pranjalbhadu171@gmail.com>
2022-07-28 12:08:20 +0530

    user-dashboard with telemetry

--- getUsers/index.ts
+++ getUsers/index.ts
@@ -1,53 +1,41 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-// import {TelemetryProvider} from "../Telemetry.Instrumentation/TelemetryProvider"
-// import {TelemetryProvider} from 'opentelemetrywrapper'
-// import {TelemetryProvider} from "../Telemetry.Instrumentation/TelemetryProvider"
 import {TelemetryProvider} from "opentelemetrywrapper"
 const axios = require("axios").default;
+
 const connstr = "InstrumentationKey=e42c6ebf-ab37-42b5-9993-6ea44d52f6bf;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://centralindia.livediagnostics.monitor.azure.com/"
 
 const tp = new TelemetryProvider("http app", "0.1.0", connstr);
-const tracer = TelemetryProvider.getTelemetryTracer()
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  // context.log("HTTP trigger function for fetching user json");
-  // let usersData;
-  // context.res.headers = { "Content-Type": "application/json" };
-  // const span = TelemetryProvider.startTracing("fetch user data json from api", undefined, 0, { attr1: "first" })
-  // TelemetryProvider.startTracingWith("test span", () => {
-  //   context.log("In test Span")
-  //   context.log(TelemetryProvider.getCurrentSpanContext())
-  //   TelemetryProvider.startTracingWith("test span child", () => {
-  //       context.log("In test Span Child")
-  //       context.log(TelemetryProvider.getCurrentSpanContext())
-  //   })
-  // })
-  // const responseMessage = await axios.get('https://jsonplaceholder.typicode.com/users')
-  // .then(users => {usersData = users.data; })
-  // .catch(err => {console.log("unable to get users")})
-  // TelemetryProvider.endTracing(span)
-  //   // console.log(userData)
-  // context.res = {
-  //     // status: 200, /* Defaults to 200 */
-  //     body: usersData
-  // };
-  let usersData: any = undefined;
-  TelemetryProvider.startTracingWith("start getUsers()", () => {
-    TelemetryProvider.startTracingWith("fetch from api", async () => {
-      const responseMessage = await axios.get('https://jsonplaceholder.typicode.com/users')
-      .then(users => {usersData = users.data})
-      .catch(err => {context.log("Unable to fetch users")})
-      context.res = {
-        headers : {
-          "Content-Type": "application/json"
-        }, 
-        body: usersData
-      }
-    })
-})
+  const span = tp.startTracing("tracing getUsers")
+  tp.addTraceEvent(span, "This is an event in getUsers")
+
+  let usersData: JSON;
+
+  const m = tp.getMessageContext()
+  m.ApplicationName = "getUsers"
+  m.CreatedBy = "Pranjal Bhadu"
+  m.UserId = "1001"
+  m.CustomProperties = {
+    version: "1.0.0"
+  }
+  const span1 = tp.startTracing("fetching dummy users", span, 0, m)
+  const responseMessage = await axios.get('https://jsonplaceholder.typicode.com/users')
+  .then(users => {usersData = users.data; })
+  .catch(err => {console.log("unable to get users")})
+  tp.endTracing(span1)
+
+  const span2 = tp.startTracing("sending data to response", span)
+  context.res = {
+    headers: { "Content-Type": "application/json" },
+    body: usersData
+  };
+  tp.endTracing(span2)
+  
+  tp.endTracing(span)
 }
 
 export default httpTrigger;
--- showDashboard/index.ts
+++ showDashboard/index.ts
@@ -1,32 +1,33 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-// import {TelemetryProvider} from "../Telemetry.Instrumentation/TelemetryProvider"
-// import {TelemetryProvider} from '../Telemetry.Instrumentation/TelemetryProvider'
 import {TelemetryProvider} from "opentelemetrywrapper"
-const connstr = "InstrumentationKey=e42c6ebf-ab37-42b5-9993-6ea44d52f6bf;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://centralindia.livediagnostics.monitor.azure.com/"
-const axios = require("axios").default;
+import { trace}  from "@opentelemetry/api";
 
+const connstr = "InstrumentationKey=e42c6ebf-ab37-42b5-9993-6ea44d52f6bf;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://centralindia.livediagnostics.monitor.azure.com/"
+
+const axios = require("axios").default;
 const tp = new TelemetryProvider("http app", "0.1.0", connstr);
-// const tracer = TelemetryProvider.getTelemetryTracer()
+
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void>{
-        const span = TelemetryProvider.startTracing("start of dashboard")
-        context.log('HTTP trigger function processed a request to show users in dashboard.');
+        const span = tp.startTracing("start of dashboard")
         let usersData: any;
         context.res.headers = { "Content-Type": "application/json" };
-        const span1 = TelemetryProvider.startTracing("fetch user data from getUsers", span)
-        const responseMessage = await axios.get('https://user-dashboard.azurewebsites.net/api/getusers')
+        const span1 = tp.startTracing("fetch user data from getUsers", span)
+        tp.addTraceEvent(span1, "this is an event in dashboard");
+        const responseMessage = await axios.get('https://user-dashboard.azurewebsites.net/api/getUsers?')
         .then(users => { 
             usersData = users.data; 
         })
         .catch(err => {console.log("unable to get users")})
-        TelemetryProvider.endTracing(span1);
-        const span2 = TelemetryProvider.startTracing("show user data on dashboard", span)
+        tp.endTracing(span1)
+
+        const span2 = tp.startTracing("show user data on dashboard", span)
         context.res = {
-            
+            headers: { "Content-Type": "application/json" },
             body: usersData
         };
-        TelemetryProvider.endTracing(span2);
-        TelemetryProvider.endTracing(span)
+        tp.endTracing(span2);
+        tp.endTracing(span)
     // return context.res
 };
 
